fb34e66abc9ea165b566d6018c34c49dc8dff4be
Dennis Miasoutov <dennis.miasoutov@mocoding.com>
2017-04-04 00:27:04 -0400

    Migrated to csproj.

--- test/Mocoding.Ofx.Client.Tests/OfxClientTests.cs
+++ test/Mocoding.Ofx.Client.Tests/OfxClientTests.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Collections.Immutable;
 using System.Threading.Tasks;
-using Mocoding.Ofx;
 using Mocoding.Ofx.Client.Exceptions;
 using Mocoding.Ofx.Client.Interfaces;
-using Mocoding.Ofx.Protocol;
 using Mocoding.Ofx.Client.Models;
 using Mocoding.Ofx.Tests;
-using Moq;
+using NSubstitute;
 using Xunit;
 
 namespace Mocoding.Ofx.Client.Tests
@@ -27,16 +24,16 @@ namespace Mocoding.Ofx.Client.Tests
                 EmbeddedResourceReader.ReadResponseAsString("accountList.sgml");
 
             var options = new OfxClientOptions(ApiUrl, "HAN", "5959", "testUserAccount", "testUserPassword");
-            var transportMock = new Mock<IOfxClientTransport>(); 
-            var utilsMock = new Mock<IUtils>();
+            var transportMock = Substitute.For<IOfxClientTransport>();
+            var utilsMock = Substitute.For<IUtils>();
 
-            transportMock.Setup(t => t.PostRequest(It.IsAny<Uri>(), It.IsAny<string>())).Returns(Task.FromResult(expectedResponse));
+            transportMock.PostRequest(Arg.Any<Uri>(), Arg.Any<string>()).Returns(Task.FromResult(expectedResponse));
 
-            utilsMock.Setup(u => u.GenerateTransactionId()).Returns("0000000000");
-            utilsMock.Setup(u => u.GetCurrentDateTime()).Returns("20150127131257");
-            utilsMock.Setup(u => u.GetClientUid(It.Is<string>(val => val == "testUserAccount"))).Returns("SomeGuidHere");
+            utilsMock.GenerateTransactionId().Returns("0000000000");
+            utilsMock.GetCurrentDateTime().Returns("20150127131257");
+            utilsMock.GetClientUid(Arg.Is<string>(val => val == "testUserAccount")).Returns("SomeGuidHere");
 
-            var client = new OfxClient(options, transportMock.Object, utilsMock.Object);
+            var client = new OfxClient(options, transportMock, utilsMock);
             var result = await client.GetAccounts();
             var account = result;
 
@@ -54,15 +51,15 @@ namespace Mocoding.Ofx.Client.Tests
                 EmbeddedResourceReader.ReadResponseAsString("creditCardTransactions.sgml");
 
             var options = new OfxClientOptions(ApiUrl, "HAN", "5959", "testUserAccount", "testUserPassword");
-            var transportMock = new Mock<IOfxClientTransport>();
-            var utilsMock = new Mock<IUtils>();
+            var transportMock = Substitute.For<IOfxClientTransport>();
+            var utilsMock = Substitute.For<IUtils>();
 
-            transportMock.Setup(t => t.PostRequest(It.IsAny<Uri>(), It.IsAny<string>())).Returns(Task.FromResult(expectedResponse));
+            transportMock.PostRequest(Arg.Any<Uri>(), Arg.Any<string>()).Returns(Task.FromResult(expectedResponse));
 
-            utilsMock.Setup(u => u.GenerateTransactionId()).Returns("0000000000");
-            utilsMock.Setup(u => u.GetCurrentDateTime()).Returns("XXXXXXXXXXXXXX");
+            utilsMock.GenerateTransactionId().Returns("0000000000");
+            utilsMock.GetCurrentDateTime().Returns("XXXXXXXXXXXXXX");
 
-            var client = new OfxClient(options, transportMock.Object, utilsMock.Object);
+            var client = new OfxClient(options, transportMock, utilsMock);
             var result = await client.GetTransactions(new Account(AccountTypeEnum.Credit, "XXXXXXXXXXXX3158"));
             var transactions = result;
 
@@ -80,15 +77,15 @@ namespace Mocoding.Ofx.Client.Tests
                 EmbeddedResourceReader.ReadResponseAsString("bankTransactions.sgml");
 
             var options = new OfxClientOptions(ApiUrl, "HAN", "5959", "testUserAccount", "testUserPassword");
-            var transportMock = new Mock<IOfxClientTransport>();
-            var utilsMock = new Mock<IUtils>();
+            var transportMock = Substitute.For<IOfxClientTransport>();
+            var utilsMock = Substitute.For<IUtils>();
 
-            transportMock.Setup(t => t.PostRequest(It.IsAny<Uri>(), It.IsAny<string>())).Returns(Task.FromResult(expectedResponse));
+            transportMock.PostRequest(Arg.Any<Uri>(), Arg.Any<string>()).Returns(Task.FromResult(expectedResponse));
 
-            utilsMock.Setup(u => u.GenerateTransactionId()).Returns("0000000000");
-            utilsMock.Setup(u => u.GetCurrentDateTime()).Returns("XXXXXXXXXXXXXX");
+            utilsMock.GenerateTransactionId().Returns("0000000000");
+            utilsMock.GetCurrentDateTime().Returns("XXXXXXXXXXXXXX");
 
-            var client = new OfxClient(options, transportMock.Object, utilsMock.Object);
+            var client = new OfxClient(options, transportMock, utilsMock);
             var result = await client.GetTransactions(new Account(AccountTypeEnum.Checking, "YYYYYYYY1924", "XXXXXXXXX"));
             var transactions = result;
 
@@ -103,34 +100,32 @@ namespace Mocoding.Ofx.Client.Tests
                 EmbeddedResourceReader.ReadResponseAsString("error.sgml");
 
             var options = new OfxClientOptions(ApiUrl, "HAN", "5959", "testUserAccount", "testUserPassword");
-            var transportMock = new Mock<IOfxClientTransport>();
-            var utilsMock = new Mock<IUtils>();
+            var transportMock = Substitute.For<IOfxClientTransport>();
+            var utilsMock = Substitute.For<IUtils>();
 
-            utilsMock.Setup(u => u.GenerateTransactionId()).Returns("0000000000");
-            utilsMock.Setup(u => u.GetCurrentDateTime()).Returns("XXXXXXXXXXXXXX");
+            utilsMock.GenerateTransactionId().Returns("0000000000");
+            utilsMock.GetCurrentDateTime().Returns("XXXXXXXXXXXXXX");
 
-            transportMock.Setup(t => t.PostRequest(It.IsAny<Uri>(), It.IsAny<string>())).Returns(Task.FromResult(expectedResponse));
+            transportMock.PostRequest(Arg.Any<Uri>(), Arg.Any<string>()).Returns(Task.FromResult(expectedResponse));
 
-            var client = new OfxClient(options, transportMock.Object, utilsMock.Object);
+            var client = new OfxClient(options, transportMock, utilsMock);
             var ex = await Assert.ThrowsAsync<OfxResponseException>(() => client.GetAccounts());
             Assert.Equal("An incorrect username/password combination has been entered. Please try again.", ex.Message);
-            
-
         }
 
         [Fact]
         public async Task ErrorRequestTest()
         {
             var options = new OfxClientOptions(ApiUrl, "HAN", "5959", "testUserAccount", "testUserPassword");
-            var transportMock = new Mock<IOfxClientTransport>();
-            var utilsMock = new Mock<IUtils>();
+            var transportMock = Substitute.For<IOfxClientTransport>();
+            var utilsMock = Substitute.For<IUtils>();
 
-            utilsMock.Setup(u => u.GenerateTransactionId()).Returns("0000000000");
-            utilsMock.Setup(u => u.GetCurrentDateTime()).Returns("XXXXXXXXXXXXXX");
+            utilsMock.GenerateTransactionId().Returns("0000000000");
+            utilsMock.GetCurrentDateTime().Returns("XXXXXXXXXXXXXX");
 
-            transportMock.Setup(t => t.PostRequest(It.IsAny<Uri>(), It.IsAny<string>())).Returns(Task.FromResult(string.Empty));
+            transportMock.PostRequest(Arg.Any<Uri>(), Arg.Any<string>()).Returns(Task.FromResult(string.Empty));
 
-            var client = new OfxClient(options, transportMock.Object, utilsMock.Object);
+            var client = new OfxClient(options, transportMock, utilsMock);
             var ex = await Assert.ThrowsAsync<FormatException>(() => client.GetAccounts());
 
             Assert.Equal("<OFX> element is not present in the response body", ex.Message);
